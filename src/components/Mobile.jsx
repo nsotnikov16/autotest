6091d576f4ddb923d5b37e958b1dd2cdf2e55220
Nikita Sotnikov <nsotnikov16@yandex.ru>
2024-07-14 14:40:43 +0500

    update

--- src/components/Mobile.jsx
+++ src/components/Mobile.jsx
@@ -1,7 +1,8 @@
 export default function Mobile() {
     return <div className="app">
         <div className="app__container app__container_mobile">
-            Конструктор автотестов не предназначен для смартфонов и планшетов
+            Конструктор автотестов не поддерживается на вашем устройстве.<br/><br/>
+            Для пользования необходимо открыть браузер на ПК.
         </div>
     </div>
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
+import YandexMetrika from './components/YandexMetrika.jsx';
 import './assets/scss/normalize.scss';
 import './assets/scss/main.scss';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
+        {import.meta.env.MODE === 'production' && <YandexMetrika />}
+        <App />
+    </>
+)
